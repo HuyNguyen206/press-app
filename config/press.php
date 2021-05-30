610af9c7f9ba09ba424c3b9591d741f23c4d1817
HuyNguyen206 <nguyenlehuyuit@gmail.com>
2021-05-30 11:18:57 +0700

    init dummy app which use for testing of press package

---
+++ config/press.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+        In case you want to override the path, please make sure the folder in the path
+        must be exist in root config of your filesystem
+    */
+    'driver' => 'file',
+    'file' => [
+        'path' => 'blog'
+    ],
+    'prefix_path' => 'press'
+];
